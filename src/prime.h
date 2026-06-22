9d2839381360ea83dbdf54b06f1c5ee1491ce9ca
Haoyang Zhou <132132792+h7777v@users.noreply.github.com>
2026-06-22 17:40:41 +1000

    Uses the ctpop instruction after sqrt S which reduces reads by 8 times.

--- src/prime.h
+++ src/prime.h
@@ -47,11 +47,11 @@ unsigned long long getPrime(unsigned long long n){
 
     uint8_t *list;
 
-    list = (uint8_t *)calloc((size >> 3) + 1, 1);
+    list = (uint8_t *)calloc(size >> 3, 1);
 
     if (list == NULL) return 1;
 
-    for(size_t i = 0;i < size;i++){
+    for(size_t i = 0;i < ubound;i++){
         if (read(i, list)) continue;
 
         c++;
@@ -61,14 +61,52 @@ unsigned long long getPrime(unsigned long long n){
             return get_number(i);
         }
 
-        if (i > ubound) continue;
-
         size_t p = 2 * i + 3;
 
         size_t start = (p * p - 3) / 2;
 
         for (size_t j = start; j < size; j += p) {
             set(j, list);
+        }
+    }
+
+    size_t byte = ubound >> 3;
+    size_t bit  = ubound & 7;
+
+    for (; bit < 8; bit++) {
+        if (!(list[byte] & (1u << bit))) {
+            c++;
+
+            if (c == n) {
+                size_t idx = (byte << 3) + bit;
+                free(list);
+                return 2 * idx + 3;
+            }
+        }
+    }
+
+    for (size_t i = byte + 1; i < ((size + 7) >> 3); i++) {
+        unsigned primes_in_byte = 8 - __builtin_popcount(list[i]);
+
+        if (c + primes_in_byte < n) {
+            c += primes_in_byte;
+            continue;
+        }
+
+        for (unsigned b = 0; b < 8; b++) {
+            size_t idx = (i << 3) + b;
+
+            if (idx >= size)
+                break;
+
+            if (!(list[i] & (1u << b))) {
+                c++;
+
+                if (c == n) {
+                    free(list);
+                    return 2 * idx + 3;
+                }
+            }
         }
     }
 
